e111bc21ea58538e313cb5b11225d9ee6ddb5248
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-08-24 08:43:58 -0700

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
+  const start = a <= b ? a : b;
+  const end = start === a ? b : a;
 
+  const sumArr = [...Array(end + 1).keys()].filter(
+    (i) => ![...Array(start).keys()].includes(i)
+  );
+  let sum = 0;
+  sumArr.forEach((i) => (sum += i));
+  return sum;
 };
 
 // Do not edit below this line
