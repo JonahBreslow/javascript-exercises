c5f7bbb65630e77b3c2add36d573de0345f1d79a
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-08-24 09:18:52 -0700

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+function round(number, decimals) {
+  let factor = 10 ** decimals;
+  return Math.round(number * factor) / factor
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (farenTemp) {
+  let temp = (5 / 9) * (farenTemp - 32) 
+  return round(temp, 1) 
 };
+
+const convertToFahrenheit = function (celciusTemp) {
+  let temp = (9 / 5) * celciusTemp + 32;
+  return round(temp, 1) 
+};
+
+console.log(convertToCelsius(69));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
