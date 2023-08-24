0c8f1449b94fe5eb01fd2bbfd17b43a572164b4f
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-08-24 08:03:57 -0700

    exercise 1, 2, 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const stringLength = string.length;
+  let result = "";
+  for (let i = stringLength - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
