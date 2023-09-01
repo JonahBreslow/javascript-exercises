1426a5fcac9844c43d9fbd955e77697f33f98be4
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-09-01 16:31:16 -0700

    finished calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  // Basic
+  return arr.reduce(add, 0);
+
+  // // Alternative 1
+  // return arr.reduce((a, b) => a + b, 0)
+
+  // // Alternative 2
+  // return arr.reduce(function(a, b) {
+  //   return a + b
+  // }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  // a to the power of b
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let result = 1;
+  for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result
 };
+
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
